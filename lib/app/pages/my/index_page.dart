8a64c4e33f79054d5515a2e43f4498ddee71af75
高宁 <2931817682@qq.com>
2019-09-10 16:08:22 +0800

    first commit

---
+++ lib/app/pages/my/index_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MyIndexPage extends StatefulWidget {
+  @override
+  _MyIndexPageState createState() => _MyIndexPageState();
+}
+
+class _MyIndexPageState extends State<MyIndexPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
